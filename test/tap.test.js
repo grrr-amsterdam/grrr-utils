7f1401e2a2cbac14d9b1989754a1c3c0d4f271e5
Harmen Janssen <harmen@grrr.nl>
2018-11-12 08:07:04 +0100

    Add tap

---
+++ test/tap.test.js
@@ -0,0 +1,15 @@
+import tap from '../src/tap';
+
+describe('tap', () => {
+  test('Executes a side-effect', () => {
+    const record = [];
+    const log = n => record.push(n);
+    const tapLog = tap(log);
+
+    expect(tapLog(42)).toEqual(42);
+    expect(record).toEqual([42]);
+
+    expect(tapLog(120)).toEqual(120);
+    expect(record).toEqual([42, 120]);
+  });
+});
