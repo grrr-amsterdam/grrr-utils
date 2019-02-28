579e1b916825db48410c0f752c7ce9306f99c175
Koen Schaft <koen@grrr.nl>
2019-02-28 18:22:22 +0100

    Add memoize function

---
+++ functions/memoize.js
@@ -0,0 +1,18 @@
+import curry from './curry';
+
+/**
+ * Store results of function calls and return the cache when input arguments
+ * are the same;
+ */
+const memoize = curry(fn => {
+  const cache = {};
+  return (...args) => {
+    const key = JSON.stringify(args);
+    if (!(key in cache)) {
+      cache[key] = fn(...args);
+    }
+    return cache[key];
+  };
+});
+
+export default memoize;
