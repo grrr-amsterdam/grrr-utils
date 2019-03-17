29dd459a681a17a2085416c18e5d3ef983d01564
Harmen Janssen <harmen@grrr.nl>
2019-03-17 14:26:18 +0100

    Refactor debounce to memoize on args
    This update allows debounce to be called multiple times with different
    arguments.
    
    `debounce(foo)(200)` will be its own debounced invocation stack, distinct
    from `debounce(foo)(100)`.
    
    Also: in the previous implementation, multiple calls would result in
    multiple promises, but most would be left hanging: only the last promise
    would resolve.
    
    This implementation ensures all promises resolve, *however*, the
    promises will all get the same value from the debounced function call.
    
    Example: https://codepen.io/harmenjanssen/pen/qvZyqr?editors=0011

--- functions/debounce.mjs
+++ functions/debounce.mjs
@@ -1,21 +1,28 @@
 /* eslint-disable func-names */
 import curry from './curry';
+import memoize from './memoize';
+
+const setTimer = memoize(() => {
+  let timer;
+  return (fn, delay) => {
+    clearTimeout(timer);
+    timer = setTimeout(fn, delay);
+  };
+});
 
 /**
- * Group function calls in a single delayed call after the last call.
- *
- * debounce :: (a -> b) -> Number -> (a -> Promise b)
+ * Creates a debounced function that delays invoking {fn} until after {delay}
+ * milliseconds have elapsed since the last time the debounced function was invoked.
+ * Returns a promise that resolves to the value returned by {fn}.
  */
 const debounce = curry(
   (fn, delay) => {
-    let timer;
-    return function (...args) {
+    return memoize(function (...args) {
+      const context = this;
       return new Promise((resolve) => {
-        const context = this;
-        clearTimeout(timer);
-        timer = setTimeout(() => resolve(fn.apply(context, args)), delay);
+        setTimer(...args)(() => resolve(fn.apply(context, args)), delay);
       });
-    };
+    });
   }
 );
 
--- test/debounce.test.js
+++ test/debounce.test.js
@@ -5,33 +5,38 @@ describe('debounce', () => {
   let debouncedFn;
 
   beforeEach(() => {
-    fn = jest.fn(() => 42);
+    fn = jest.fn((x) => x * 2);
     debouncedFn = debounce(fn, 1000);
   });
 
   jest.useFakeTimers();
 
   test('Should debounce function calls', () => {
-    expect.assertions(3);
+    expect.assertions(4);
 
-    const promise = debouncedFn().then(x => expect(x).toEqual(42));
+    const promises = [
+      debouncedFn(21).then(x => expect(x).toEqual(42)),
+      debouncedFn(200).then(x => expect(x).toEqual(400)),
+    ];
 
     expect(fn).toHaveBeenCalledTimes(0);
 
     jest.runAllTimers();
-    expect(fn).toHaveBeenCalledTimes(1);
-    return promise;
+    expect(fn).toHaveBeenCalledTimes(2);
+    return Promise.all(promises);
   });
 
   test('Should only call it once', () => {
+    const promises = [];
     for (let i = 0; i < 100; i += 1) {
-      debouncedFn();
+      promises.push(debouncedFn());
       jest.advanceTimersByTime(1);
     }
     expect(fn).toHaveBeenCalledTimes(0);
 
     jest.runAllTimers();
     expect(fn).toHaveBeenCalledTimes(1);
+    return Promise.all(promises);
   });
 
   test('Should call function with given arguments', () => {
