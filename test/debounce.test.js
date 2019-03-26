26038ecd1b21d94110e6c0f75cd2fd99877e2780
Koen Schaft <koen@grrr.nl>
2019-03-26 09:44:28 +0100

    Adjust debounce test

--- test/debounce.test.js
+++ test/debounce.test.js
@@ -1,44 +1,55 @@
 import debounce from '../functions/debounce';
 
+const THRESHOLD = 1000;
+
 describe('debounce', () => {
   let fn;
   let debouncedFn;
 
   beforeEach(() => {
     fn = jest.fn((x) => x * 2);
-    debouncedFn = debounce(fn, 1000);
+    debouncedFn = debounce(fn, THRESHOLD);
   });
 
   jest.useFakeTimers();
 
   test('Should debounce function calls', () => {
-    expect.assertions(3);
+    expect.assertions(6);
 
-    const promise = debouncedFn(21).then(x => expect(x).toEqual(42));
+    const promises = [
+      debouncedFn(21).then(x => expect(x).toEqual(42)),
+    ];
 
     expect(fn).toHaveBeenCalledTimes(0);
-
-    jest.runAllTimers();
+    jest.advanceTimersByTime(THRESHOLD);
     expect(fn).toHaveBeenCalledTimes(1);
-    return promise;
+
+    promises.push(debouncedFn(3).then(x => expect(x).toEqual(6)));
+
+    expect(fn).toHaveBeenCalledTimes(1);
+    jest.advanceTimersByTime(THRESHOLD);
+    expect(fn).toHaveBeenCalledTimes(2);
+
+    return Promise.all(promises);
   });
 
   test('Should only call it once', () => {
     let promise;
-    for (let i = 0; i < 100; i += 1) {
+    for (let i = 0; i < THRESHOLD; i += 1) {
       promise = debouncedFn();
       jest.advanceTimersByTime(1);
     }
-    expect(fn).toHaveBeenCalledTimes(0);
 
-    jest.runAllTimers();
+    expect(fn).toHaveBeenCalledTimes(0);
+    jest.advanceTimersByTime(THRESHOLD);
     expect(fn).toHaveBeenCalledTimes(1);
+
     return promise;
   });
 
   test('Should call function with given arguments', () => {
     debouncedFn('foo');
-    jest.runAllTimers();
+    jest.advanceTimersByTime(THRESHOLD);
     expect(fn).toHaveBeenCalledWith('foo');
   });
 });
